74313d30f9b56ae125ab29d1e3807a1aac407e29
Ste <cowley05@hotmail.co.uk>
2014-09-17 00:28:12 +0100

    Ste - trying to fix heroku running

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                            [environ "0.5.0"]]
             :plugins [[lein-ring "0.8.11"]
                       [environ/environ.lein "0.2.1"]]
+            :min-lein-version "2.0.0"
             :hooks [environ.leiningen.hooks]
             :ring {:handler paintball-clj.handler/app}
             :uberjar-name "paintball-clj-standalone.jar"
